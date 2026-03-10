2322be7a9cf95ded171c33e7bc947a33dd4e37ca
GURUPRASATH K C <kcguruprasath@gmail.com>
2026-03-10 19:48:23 +0530

    Implement is Subsequence method in Solution class

---
+++ Is Subsequence.java
@@ -0,0 +1,16 @@
+// Problem: Concatenation of Array
+//link:https://neetcode.io/problems/is-subsequence/question
+// Platform: neetcode.io
+// Difficulty: Easy
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0, j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+        return i == s.length();
+    }
+}
